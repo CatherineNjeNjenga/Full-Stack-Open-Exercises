d7ff9c23a07bcb0ff071e9aab569f8613ad93a67
Catherine Njenga <freshiah.catherine@gmail.com>
2024-11-27 09:19:17 +0300

    adding user auth

---
+++ backend_notes/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true // this ensures uniqueness of username
+  },
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
+  ]
+})
+
+userSchema.set('toJSON', (document, returnedObject) => {
+  returnedObject.id = returnedObject._id.toString(),
+  delete returnedObject._id,
+  delete returnedObject.__v,
+  // the passwordHash should not be revealed
+  delete returnedObject.passwordHash
+})
+
+module.exports = mongoose.model('User', userSchema)
